66276d4a16f92f9d500e2a8e44d7bd4e889df302
Crowdbotics <engineering@crowdbotics.com>
2023-05-05 23:08:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sparkling-mountain--81565",
+  url: "https://sparkling-mountain--81565.botics.co",
+  api: "https://sparkling-mountain--81565.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
